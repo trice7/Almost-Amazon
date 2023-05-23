59a98a841d5e81bec5ad683c11099b4811c2babd
Thomas Rice <ricethomasj@gmail.com>
2023-05-22 21:20:28 -0500

    added GET, FILTER, and DEL functionality for books

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,5 +1,8 @@
 import { signOut } from '../utils/auth';
-
+import { booksOnSale, getBooks } from '../api/bookData';
+import { showBooks } from '../pages/books';
+import { getAuthors } from '../api/authorData';
+import { showAuthors } from '../pages/authors';
 // navigation events
 const navigationEvents = () => {
   // LOGOUT BUTTON
@@ -8,12 +11,12 @@ const navigationEvents = () => {
 
   // TODO: BOOKS ON SALE
   document.querySelector('#sale-books').addEventListener('click', () => {
-    console.warn('CLICKED SALE BOOKS');
+    booksOnSale().then((item) => showBooks(item));
   });
 
   // TODO: ALL BOOKS
   document.querySelector('#all-books').addEventListener('click', () => {
-    console.warn('CLICKED ALL BOOKS');
+    getBooks().then(showBooks);
   });
 
   // FIXME: STUDENTS Create an event listener for the Authors
@@ -21,6 +24,7 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
+    getAuthors().then((item) => showAuthors(item));
     console.warn('CLICKED AUTHORS');
   });
 

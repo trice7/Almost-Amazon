cc72361cefce2a261bf7ed2cb793752fff8f722d
Thomas Rice <ricethomasj@gmail.com>
2023-05-26 17:44:01 -0500

    added Author functionality and display functionality

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,25 @@
 // for merged promises
+
+import { getAuthorBooks, getSingleAuthor } from './authorData';
+import { getSingleBook } from './bookData';
+
+const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  // GET Single Book
+
+  getSingleBook(firebaseKey).then((bookObject) => {
+    getSingleAuthor(bookObject.author_id)
+      .then((authorObject) => resolve({ ...bookObject, authorObject }));
+  }).catch(reject);
+});
+
+const getAuthorDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleAuthor(firebaseKey).then((authorObject) => {
+    getAuthorBooks(authorObject.firebaseKey)
+      .then((booksObject) => resolve({ ...authorObject, booksObject }));
+  }).catch(reject);
+});
+
+export {
+  getBookDetails,
+  getAuthorDetails
+};
